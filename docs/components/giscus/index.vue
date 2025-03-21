aa9131c4439ff29c82dbb8c91fe716208492ea18
yinming <1301680879@qq.com>
2025-03-22 00:47:33 +0800

    fix: giscus评论组件支持

--- docs/components/giscus/index.vue
+++ docs/components/giscus/index.vue
@@ -1,27 +1,9 @@
 <template>
-  <!--正常配置-->
-  <!-- <Giscus
-    :key="route.path"
+  <Giscus
     repo="2sky2night/note-press"
     repo-id="R_kgDOKpx2iw"
-    category="Announcements"
-    category-id="DIC_kwDOKpx2i84Catge"
-    mapping="pathname"
-    strict="0"
-    reactions-enabled="1"
-    emit-metadata="0"
-    input-position="bottom"
-    :theme="isDark"
-    lang="zh-CN"
-    crossorigin="anonymous"
-    async /> -->
-
-  <!--若此仓库需要迁移到其他仓库中去，需要修改repo以及id-->
-  <Giscus
-    repo="2sky2night/note-blog"
-    repo-id="R_kgDOKqOR9g"
-    category="Announcements"
-    category-id="DIC_kwDOKqOR9s4CavqB"
+    category="Show and tell"
+    category-id="DIC_kwDOKpx2i84Catgi"
     mapping="pathname"
     strict="0"
     reactions-enabled="1"
