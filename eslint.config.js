99d724974e228312774cfac3653413df3feb76a9
yinming <1301680879@qq.com>
2025-03-21 23:16:13 +0800

    chore: 代码检查eslint支持

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  ...pluginVue.configs["flat/recommended"],
+  tseslint.configs.recommended,
+]);
