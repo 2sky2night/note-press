5d9ab79573a6647f05a25ef4ed6bec7794e5124b
yinming <1301680879@qq.com>
2025-03-21 22:54:23 +0800

    chore: 配置prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+
+/** 
+ * pretttier 配置文件
+ * @type {import('prettier').Config}
+ */
+const config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: 'all',
+  tabWidth: 2,
+}
+
+module.exports = config;
